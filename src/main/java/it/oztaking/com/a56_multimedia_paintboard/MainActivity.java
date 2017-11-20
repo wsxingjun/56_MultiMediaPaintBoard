6d21f5eaefe25d702f6aa5f6a9246e5a3c2b62b5
wsxingjun <1185889308@qq.com>
2017-11-20 14:31:22 +0800

    v1：以图片为背景 可以连续画画 可以选择笔的颜色 笔的宽度

---
+++ src/main/java/it/oztaking/com/a56_multimedia_paintboard/MainActivity.java
@@ -0,0 +1,116 @@
+package it.oztaking.com.a56_multimedia_paintboard;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Bitmap bitmap;
+    private Bitmap copyBitmap;
+    private Paint paint;
+    private ImageView iv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //1.将图片设置到imageView中；
+        iv = (ImageView) findViewById(R.id.iv);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.backpic);
+
+        //2.创建模板
+        copyBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+        //3.以copyBitmap为模板，创建画板
+        final Canvas canvas = new Canvas(copyBitmap);
+        //4.创建画笔
+        paint = new Paint();
+
+
+        //开始作画
+        canvas.drawBitmap(bitmap, new Matrix(), paint);
+        iv.setImageBitmap(copyBitmap);
+
+        iv.setOnTouchListener(new View.OnTouchListener(){
+            int startX = 0;
+            int startY = 0;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //【6】获取触摸的事件
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        //【7】获取手指按下坐标
+                        startX = (int) event.getX();
+                        startY = (int) event.getY();
+                        System.out.println("按下-x:" + startX + "--y:" + startY);
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        //【7】获取手指移动
+                        int stopX = (int) event.getX();
+                        int stopY = (int) event.getY();
+                        System.out.println("移动-x:" + stopX + "--y:" + stopY);
+
+                        //【8】划线
+                        canvas.drawLine(startX, startY, stopX, stopY, paint);
+
+                        //【9】更新起点坐标
+                        startX = stopX;
+                        startY = stopY;
+
+                        iv.setImageBitmap(copyBitmap);
+
+                        break;
+                }
+                //return false;
+                return true;
+            }
+        });
+
+}
+
+    public void ClickPaintRed(View v) {
+
+        paint.setColor(Color.RED);
+    }
+
+    public void ClickPaintBold(View v) {
+        paint.setStrokeWidth(15);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
